40e62a7dfede3c1c95ceb94a3bd4138ca11f9a34
sra <sara.habib48@gmail.com>
2023-12-30 01:17:01 +0200

    form validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,25 @@
+import { data } from "autoprefixer";
 import React from "react";
+import { useForm } from "react-hook-form";
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState:{errors, isSubmitting},
+    reset,
+    getValues,
+    
+  } = useForm();
+
+  const onSubmit = async(data) => {
+    
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+    reset();
+  };
+
+
     return(
 <section class="h-screen">
   <div class="container h-full px-6 py-24">
@@ -15,14 +35,20 @@ const Register = () => {
 
       {/* <!-- Right column container with form --> */}
       <div class="md:w-8/12 lg:ml-6 lg:w-5/12">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           {/* <!-- Email input --> */}
           <div class="relative mb-6" data-te-input-wrapper-init>
             <input
-              type="text"
+             {...register("email", {required: "Email is required"})}
+              type="email"
               class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
               id="exampleFormControlInput3"
-              placeholder="Email address" />
+              placeholder="Email address"
+               />
+              {errors.email && (
+                 <p className="text-red-500">{errors.email.message}</p>
+                )}
+
             <label
               for="exampleFormControlInput3"
               class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -33,10 +59,18 @@ const Register = () => {
           {/* <!-- Password input --> */}
           <div class="relative mb-6" data-te-input-wrapper-init>
             <input
+              {...register("password", {
+                required: "Password is required" ,
+                minLength: {
+                  value: 10,
+                  message: "Password must be at least 10 characters"
+                },
+              })}
               type="password"
               class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
               id="exampleFormControlInput33"
               placeholder="Password" />
+              {errors.password && (<p className="text-red-500">{errors.password.message}</p>)}
             <label
               for="exampleFormControlInput33"
               class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -70,6 +104,7 @@ const Register = () => {
 
           {/* <!-- Submit button --> */}
           <button
+            disabled={isSubmitting}
             type="submit"
             class="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
             data-te-ripple-init
