c3bef7f86d34a3b96761af62a21ae2bfccbc89fc
sra <sara.habib48@gmail.com>
2024-01-22 18:30:50 +0200

    Mobile Nav deep green

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -167,7 +167,7 @@ const Nav = () => {
       >
         <div className="fixed inset-0 z-10" />
 
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#EADDCA] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#1d4d16] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Booking.com</span>
