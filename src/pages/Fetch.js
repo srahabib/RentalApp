228553167f5f1d5860c32ce39931f4975e062c6a
sra <sara.habib48@gmail.com>
2024-02-18 18:22:34 +0200

    Payment

--- src/pages/Fetch.js
+++ src/pages/Fetch.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
-export const getStaticProps = async () => {
-    const res = await fetch('http://ec2-54-158-135-122.compute-1.amazonaws.com:3000/user/all');
-    const data = await res.json();
+// export const getStaticProps = async () => {
+//     const res = await fetch('http://ec2-54-158-135-122.compute-1.amazonaws.com:3000/user/all');
+//     const data = await res.json();
 
-    return {
-        props: { users : data }
-    }
-}
+//     return {
+//         props: { users : data }
+//     }
+// }
 
 const Fetch = ({ users }) => {
     return (    
         <div>
             <h1>Fetch</h1>
 
-            {users.map(user => (
+            {/* {users.map(user => (
                 <div key={user.id}>
                     <h3>{user.email}</h3>
-                    <p>{user.address}</p>
                 </div>
-            ))}
+            ))} */}
         </div>
     );
 }
