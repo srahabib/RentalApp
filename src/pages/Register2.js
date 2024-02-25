b4a62ec5d2c5a917f2331a99b917ce648bb72f18
sra <sara.habib48@gmail.com>
2024-02-25 20:13:05 +0200

    Register user

--- src/pages/Register2.js
+++ src/pages/Register2.js
@@ -1,181 +1,245 @@
-// components/RegisterForm.js
-import { format, differenceInYears } from "date-fns";
+import { useForm } from "react-hook-form";
+import React from "react";
+import { useRouter } from "next/router";
 
-import React, { useState } from "react";
+const Register = () => {
+  const router = useRouter();
 
-const RegisterForm = () => {
-  const [formData, setFormData] = useState({
-    firstname: "",
-    lastname: "",
-    phone: "",
-    gender: "",
-    birthdate: "",
-    email: "",
-    password: "",
-    age: 0,
-  });
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors, isSubmitting },
+  //   reset,
+  // } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  // // const onSubmit = async (data, e) => {
+  // //   e.preventDefault();
 
-    if (name === "birthdate") {
-      const birthdate = new Date(value);
-      const age = differenceInYears(new Date(), birthdate);
-      setFormData((prevData) => ({ ...prevData, age }));
+  // //   const response = await fetch("https://rentor-b.onrender.com/user/create", {
+  // //     method: "POST",
+  // //     headers: { "Content-Type": "application/json" },
+  // //     body: JSON.stringify(data),
+  // //   });
+
+  // //   if (response.ok) {
+  // //     console.log("User registered successfully!");
+  // //     router.push("/");
+  // //   } else {
+  // //     // Handle registration errors
+  // //     console.error("Failed to register user");
+  // //   }
+  // // };
+  // const onSubmit = async (data, e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     const response = await fetch(
+  //       "https://rentor-b.onrender.com/user/create",
+  //       {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify(data),
+  //       }
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error(`Failed to register user, status: ${response.status}`);
+  //     }
+  //     console.log("User registered successfully!");
+  //     router.push("/");
+  //   } catch (error) {
+  //     console.error("Error during registration:", error.message);
+  //   }
+  // };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      data.birthdate = new Date(data.birthdate).toISOString();
+      const response = await fetch(
+        "https://rentor-b.onrender.com/user/create",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Failed to register user, status: ${response.status}`);
+      }
+      // data.birthdate = new Date(birthdate).toISOString();
+      console.log("User registered successfully!");
+      router.push("/Login");
+    } catch (error) {
+      console.error("Error during registration:", error.message);
     }
-
-    // if (name === "password" && value.length < 8) {
-    //   // Password is too short
-    //   // You can display an error message or handle it accordingly
-    //   console.log("Password must be at least 8 characters long");
-    //   return;
-    // }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission (e.g., send data to a server)
-    console.log("Form submitted:", formData);
   };
 
   return (
     <form
-      onSubmit={handleSubmit}
-      className="max-w-md mx-auto mt-4 p-4 bg-white rounded shadow-md"
+      onSubmit={handleSubmit(onSubmit)}
+      className="max-w-md mx-auto mt-4 p-3 bg-gray-50 rounded shadow-xl"
     >
-      <div className="mb-4">
-        <label
-          htmlFor="firstname"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          First Name
-        </label>
+      {/* Form fields */}
+      <div className="mb-4 text-center">
+        <p className="text-gray-700 text-base font-bold mb-2">Register</p>
+
+        {/* first name */}
         <input
-          type="text"
-          id="firstname"
-          name="firstname"
-          value={formData.firstname}
-          onChange={handleChange}
+          {...register("firstName", {
+            required: "First Name is Required",
+          })}
+          name="firstName"
+          type="firstName"
+          placeholder="First Name"
           className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
-          required
         />
+        {errors.firstName && (
+          <p className="text-red-700 w-auto mt-1 text-center">{`${errors.firstName.message}`}</p>
+        )}
       </div>
-      <div className="mb-4">
-        <label
-          htmlFor="lastname"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Last Name
-        </label>
+      <div className="mb-4 text-center">
+        {/* last name */}
         <input
-          type="text"
-          id="lastname"
-          name="lastname"
-          value={formData.lastname}
-          onChange={handleChange}
+          {...register("lastName", {
+            required: "Last Name is Required",
+          })}
+          name="lastName"
+          type="lastName"
+          placeholder="Last Name"
           className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
-          required
         />
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
-            required
-          />
-        </div>
+        {errors.lastName && (
+          <p className="text-red-700 w-auto mt-1 text-center">{`${errors.lastName.message}`}</p>
+        )}
       </div>
-      <div className="mb-4">
-        <label
-          htmlFor="password"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Password
-        </label>
+      {/* email  */}
+      <div className="mb-4 text-center">
         <input
-          type="password"
-          id="password"
+          {...register("email", {
+            required: "Email is required",
+          })}
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
+        />
+        {errors.email && (
+          <p className="text-red-700 w-auto mt-1 text-center">{`${errors.email.message}`}</p>
+        )}
+      </div>
+      {/* password */}
+      <div className="mb-4 text-center">
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
           name="password"
-          value={formData.password}
-          onChange={handleChange}
+          type="password"
+          placeholder="Password"
           className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
-          required
         />
+        {errors.password && (
+          <p className="text-red-700 w-auto mt-1 text-center">{`${errors.password.message}`}</p>
+        )}
       </div>
+      {/* gender */}
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2">
-          Gender:
+          <p className="pl-1">Gender</p>
         </label>
-        <label className="inline-flex items-center mr-4">
+        <div className="pl-1 mb-4">
           <input
-            type="radio"
+            {...register("gender", {
+              required: "Gender is Required",
+            })}
             name="gender"
-            value="male"
-            checked={formData.gender.includes("male")}
-            onChange={handleChange}
-            className="form-checkbox text-blue-500 focus:outline-none focus:shadow-outline-blue"
+            type="radio"
+            value={"male"}
           />
-          <span className="ml-2">Male</span>
-        </label>
-        <label className="inline-flex items-center mr-4">
+          <span className="ml-2 text-gray-700">Male</span>
           <input
-            type="radio"
+            {...register("gender", {
+              required: "Gender is Required",
+            })}
             name="gender"
-            value="female"
-            checked={formData.gender.includes("female")}
-            onChange={handleChange}
-            className="form-checkbox text-pink-500 focus:outline-none focus:shadow-outline-pink"
+            type="radio"
+            value={"Female"}
+            className="ml-4"
           />
-          <span className="ml-2">Female</span>
-        </label>
+          <span className="ml-2 text-gray-700">Female</span>
+          {errors.gender && (
+            <p className="text-red-700 w-auto mt-1 text-center">{`${errors.gender.message}`}</p>
+          )}
+        </div>
+        {/* birthdate */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            <p className="pl-1">Birthdate</p>
+          </label>
+          <input
+            {...register("birthdate", {
+              required: "Birthdate is Required",
+            })}
+            name="birthdate"
+            type="date"
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
+          />
+          {errors.birthdate && (
+            <p className="text-red-700 w-auto mt-1 text-center">{`${errors.birthdate.message}`}</p>
+          )}
+        </div>
+        <div className="mb-4 text-center">
+          {/* Phone Numbers */}
+          <input
+            {...register("phone", {
+              required: "Phone Number is Required",
+              maxLength: {
+                value: 11,
+                message: "Phone Number must be 11 characters",
+              },
+              minLength: {
+                value: 11,
+                message: "Phone Number must be 11 characters",
+              },
+            })}
+            name="phone"
+            type="phone"
+            placeholder="Phone"
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
+          />
+          {errors.phone && (
+            <p className="text-red-700 w-auto mt-1 text-center">{`${errors.phone.message}`}</p>
+          )}
+        </div>
       </div>
-      <div className="mb-4">
-        <label
-          htmlFor="birthdate"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Birthdate
-        </label>
+      <div className="mb-4 text-center">
         <input
-          type="date"
-          id="birthdate"
-          name="birthdate"
-          value={formData.birthdate}
-          onChange={handleChange}
+          {...register("address", {
+            required: "address is required",
+          })}
+          name="address"
+          type="address"
+          placeholder="address"
           className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
-          required
         />
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="phone"
-          className="block text-gray-700 text-sm font-bold mb-2"
-        >
-          Phone
-        </label>
-        <input
-          type="phone"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          className="w-full px-3 py-2 border rounded focus:outline-none focus:shadow-outline-blue"
-          required
-        ></input>
+        {errors.address && (
+          <p className="text-red-700 w-auto mt-1 text-center">{`${errors.address.message}`}</p>
+        )}
       </div>
       <button
+        disabled={isSubmitting}
         type="submit"
-        className="w-full bg-amber-600 text-white p-2 rounded focus:outline-none focus:shadow-outline-blue"
+        className="w-full bg-amber-600 text-white p-2 rounded focus:outline-none focus:shadow-outline-blue "
       >
         Sign Up
       </button>
@@ -183,4 +247,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default Register;
