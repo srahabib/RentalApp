2bf7a298ddd38627f23137b4bf84212157e4c546
sra <sara.habib48@gmail.com>
2024-03-19 01:26:44 +0200

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import { useRouter } from 'next/router';
 const Navbar = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track if the user is logged in
     const [userPhoto, setUserPhoto] = useState(null); // State to store user's photo
+    const [showDropdown, setShowDropdown] = useState(false); // State to manage dropdown visibility
+    const [showNav, setShowNav] = useState(false); // State to manage mobile navigation visibility
     const router = useRouter();
 
     useEffect(() => {
@@ -25,194 +27,123 @@ const Navbar = () => {
         if (parts.length === 2) return parts.pop().split(';').shift();
     };
 
-
     // Function to fetch user photo
     const fetchUserPhoto = async () => {
         // Assuming you have a static URL for the user icon
         const userIconUrl = '/images/Host.jpg';
         setUserPhoto(userIconUrl);
-      
     };
 
-    const showNav = () => {
-        const nav = document.getElementById('navbarSupportedContent13');
-        if (nav.style.display === 'none') {
-            nav.style.display = 'block';
-        } else {
-            nav.style.display = 'none';
-        }
-    };
-
-    // Function to handle logout
     const handleLogout = () => {
         // Clear the access token
         document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         window.location.reload();
     };
 
-    const opendropdown = () => {
-        const dropdown = document.getElementById('user-dropdown');
-        if (dropdown.style.display === 'none') {
-            dropdown.style.display = 'block';
-        } else {
-            dropdown.style.display = 'none';
-        }
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
     };
 
+    const toggleNav = () => {
+        setShowNav(!showNav);
+    };
 
     return (
-
-<section>
-<nav
-    class=" p-2 lg:p-20 relative flex w-full flex-nowrap items-center justify-between bg-white py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:flex-wrap lg:justify-start lg:py-4"
-    data-te-navbar-ref>
-    <div class="flex w-full flex-wrap items-center justify-between px-3">
-        <button onClick={showNav} data-collapse-toggle="navbar-solid-bg"
-        class=" block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none dark:text-neutral-200 lg:hidden"
-        type="button"
-        data-te-collapse-init
-        data-te-target="#navbarSupportedContent13"
-        aria-controls="navbarSupportedContent13"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="[&>svg]:w-7">
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="h-7 w-7">
-            <path
-                fill-rule="evenodd"
-                d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                clip-rule="evenodd" />
-            </svg>
-        </span>
-        </button>
-
-        <div
-        class="!visible mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"
-        id="navbarSupportedContent13"
-        data-te-collapse-item>
-        <div class="ml-2">
-            <a
-            class="text-xl text-neutral-800 dark:text-neutral-200"
-            href="#"
-            ></a
-            >
-        </div>
-        <ul
-            class="list-style-none mr-auto flex flex-col pl-0 lg:mt-1 lg:flex-row"
-            data-te-navbar-nav-ref>
-            <li
-            class="my-4 pl-2 lg:my-0 lg:pl-2 lg:pr-1"
-            data-te-nav-item-ref>
-            <a
-                class="active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                aria-current="page"
-                href="#"
-                data-te-nav-link-ref
-                >
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="images/rentorLogo4.svg" className="h-8" alt="Flowbite Logo" />
                 </a>
-            
-            </li>
 
-            <img className="hidden lg:block w-5 h-5" src="/images/rentorLogo4.svg" alt="" />
-            <li
-            class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-            data-te-nav-item-ref>
-            <a 
-                class=" pl-0 lg:pl-5 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                href="/"
-                data-te-nav-link-ref
-                >Home</a
-            >
-            </li>
-            
-            <li
-            class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-            data-te-nav-link-ref>
-            <a href="/Search"
-                class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                >Apartments</a
-            >
-            </li>
-            <li
-            class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-            data-te-nav-link-ref>
-            <a href='/Contact'
-                class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                >Contact</a
-            >
-            </li>
-            <li
-            class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-            data-te-nav-link-ref>
-            <a  href='/Login'
-                class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                > Login </a
-            >
-            </li>
-            
-        </ul>
-        <div class="w-[300px] lg:pr-2">
-            <div class="relative flex w-full flex-wrap items-stretch">
-            <input
-                type="search"
-                class="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none motion-reduce:transition-none dark:border-neutral-500 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                placeholder="Search"
-                aria-label="Search"
-                aria-describedby="button-addon3" />
+                <div className="hidden md:flex items-center space-x-4 ">
+                    <ul className="flex flex-row items-center space-x-4">
+                        <li>
+                            <a href="#" className="text-gray-700 dark:text-white">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-700 dark:text-white">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-700 dark:text-white">Apartments</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-700 dark:text-white">Contact</a>
+                        </li>
 
-            <button
-                class="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none motion-reduce:transition-none"
-                type="button"
-                id="button-addon3"
-                data-te-ripple-init>
-                Search
-            </button>
+                        {!isLoggedIn && (
+                        <li>
+                            <a href="/Login" className="text-gray-700 dark:text-white">Login</a>
+                        </li>
+                    )}
+
+                    </ul>
+
+
+                </div>
+
+                {isLoggedIn && (
+                        <div className="relative ml-4">
+                            <button onClick={toggleDropdown} type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false">
+                                <span className="sr-only">Open user menu</span>
+                                <img className="w-8 h-8 rounded-full" src={userPhoto} alt="user photo" />
+                            </button>
+                            {showDropdown && (
+                                <div className="absolute right-0 mt-2 z-50 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                                    <div className="px-4 py-3">
+                                        <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                                        <span className="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                                    </div>
+                                    <ul className="py-2" aria-labelledby="user-menu-button">
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                                        </li>
+                                        <li>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                        </li>
+                                        <li>
+                                            <a onClick={handleLogout} href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            )}
+                        </div>
+                    )}
+
+
+                <div className="md:hidden">
+                    <button onClick={toggleNav} className="block text-gray-700 dark:text-white">
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                        </svg>
+                    </button>
+                </div>
             </div>
-        </div>
 
-    </div>
-</div>
-    
-    <div className={!isLoggedIn ? "mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto" : "mt-2 flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"}>
-            {isLoggedIn && (
-        <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button onClick={opendropdown} type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-        <span class="sr-only">Open user menu</span>
-        <img class="w-8 h-8 rounded-full" src={userPhoto} alt="user photo"/>
-      </button>
+            {/* Mobile Navigation */}
+            <div className={`md:hidden ${showNav ? 'block' : 'hidden'}`}>
+                
+                <ul className="flex flex-col items-center mt-4 space-y-4">
+                    <li>
+                        <a href="#" className="text-gray-700 dark:text-white">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-gray-700 dark:text-white">About</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-gray-700 dark:text-white">Apartments</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-gray-700 dark:text-white">Contact</a>
+                    </li>
 
-      <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-        <div class="px-4 py-3">
-          <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-          <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-        </div>
-        <ul class="py-2" aria-labelledby="user-menu-button">
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-          </li>
-          <li>
-            <a onClick={handleLogout} href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-          </li>
-        </ul>
-      </div>
-    </div>    
-            )}
-
+                </ul>
             </div>
-</nav>
-
-    </section>
-
+        </nav>
     );
-    };
+};
 
 export default Navbar;
