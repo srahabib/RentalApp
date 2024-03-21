84b92fe720d91150c9c54bc4731714031529c411
sra <sara.habib48@gmail.com>
2024-03-21 21:33:08 +0200

    Map

---
+++ src/pages/testMap.js
@@ -0,0 +1,18 @@
+import { React, useState } from "react";
+import SearchLocationInput from "@/components/Map/GooglePlacesApi";
+import MapComponent from "@/components/Map/MapComponent";
+
+const testMap = () => {
+  const [selectedLocation, setSelectedLocation] = useState({
+    lat: 31.2001,
+    lng: 29.9187,
+  });
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <SearchLocationInput setSelectedLocation={setSelectedLocation} />
+      <MapComponent selectedLocation={selectedLocation} />
+    </div>
+  );
+};
+
+export default testMap;
