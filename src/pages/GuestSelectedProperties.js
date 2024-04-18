27190c5aea5a7209939d58a144bc934fc66cd848
Rewan <143016191+RewanRamadan@users.noreply.github.com>
2024-04-19 00:39:55 +0200

    Add files via upload

---
+++ src/pages/GuestSelectedProperties.js
@@ -0,0 +1,168 @@
+import React from "react";
+import HouseBlock from "@/components/HouseBlock/HouseBlock";
+import Pagination from "@/components/Pagination/Pagination";
+
+const GuestSelectedProperties = () => {
+    return (
+
+<main>
+<div
+            class="h-full ml-10 mr-10 flex flex-col bg-white
+            :bg-gray-500 shadow-xl ">
+            
+            <div class="bg-orange-400 shadow-lg pb-3 rounded-b-2xl">
+                <div
+                    class="flex  flex-row flex-wrap -mx-3 rounded-b-3xl bg-white light:bg-gray-500 space-y-5  items-left pl-10 py-7 ">
+                    <img class="h-20 w-20  rounded-full"
+                        src="images/Guest.jpg"
+                        alt="User" />
+                    <a href="#"> <span
+                            class="text-h1 text-2xl font-bold ml-10">John Doe</span></a>
+                            <div class="felx mx-auto pt-10 w-100  ">
+                            <p><a target="_blank" class="hover:text-blue-500" rel="noopener"></a></p>
+                            {/* <!--description--> */}
+                            <p class="hidden mt-10  sm:block">Programmer, Mom, Passionate about architecture</p>
+                            {/* <!--social--> */}
+                <a href=""> <button
+                        class="p-2 shadow-lg ml-10 tr-300 w-100 font-medium  bg-orange-500 rounded-md hover:bg-orange-600 text-white">Edit profile</button></a>
+            
+                
+        </div>
+        </div>
+                
+                <div
+                    class="grid px-2 py-2  items-center justify-around grid-cols-3 gap-4 divide-x divide-solid ">
+                    <div class="col-span-1 flex flex-col items-center ">
+                        <span class="text-md font-bold 0 text-white">Selected Properties</span>
+                        
+                        <span class="text-lg font-bold text-white">6</span>
+
+                    </div>
+                    <div class="col-span-1 px-3 flex flex-col items-center ">
+                        <span class="text-md font-bold text-white">
+                            Bookings</span>
+                        <span class="text-lg font-bold text-white">1</span>
+                    </div>
+                    <div class="col-span-1 px-3 flex flex-col items-center ">
+                    <span class="text-md font-medium text-white">More properties</span>
+
+                        <span class="text-lg font-bold text-white">
+                            546</span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div
+                class="grid rounded-2xl divide-y divide-dashed hover:divide-solid  justify-evenly bg-gray-50 dark:bg-gray-300  mt-10 grid-cols-4">
+                <div class="col-span-1  p-2 ">
+                    <div class="flex flex-col items-center ">
+                        <a href=""> <button
+                                class="tr-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                class="w-10 h-10 ml-5 text-gray-500">
+                                <path stroke-linecap="round"
+                                stroke-linejoin="round" 
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+</svg>
+
+                                <span class="text-md font-medium">Favourites</span>
+                            </button></a>
+                    </div>
+                </div>
+
+
+                <div class="col-span-1  p-2 bg-gray-200">
+                    <div class="flex flex-col items-center ">
+                        <a href=""> <button
+                                class="tr-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                class="w-10 h-10 ml-10 text-gray-500">
+                                <path stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+
+                                <span class="text-md font-medium">Selected Properties</span>
+                            </button></a>
+                    </div>
+                </div>
+
+
+                <div class="col-span-1  p-2">
+                    <div class="flex flex-col items-center ">
+                        <a href=""> <button
+                                class="tr-300">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    class="h-10 w-10 ml-3 text-gray-500" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor"
+                                    stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                </svg>
+                                <span class="text-md font-medium">Contacts</span>
+                            </button></a>
+                    </div>
+                </div>
+                <div class="col-span-1  p-2">
+                    <div class="flex flex-col items-center">
+                        <a href=""> <button
+                                class="tr-300">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    class="h-10 w-10 ml-4 text-gray-500 items-center" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor"
+                                    stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                                </svg>
+                                <span class="text-md font-medium">Notifications</span>
+                            </button></a>
+                    </div>
+                </div>
+                </div>
+                </div>
+
+                
+                
+        
+
+
+
+                
+                <div class="flex-shrink max-w-full w-full px-3 mt-10 ml-10">
+                    <div class="flex ml-10  px-5 py-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                class="w-8 h-8 mt-3 text-gray-500">
+                                <path stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+        
+
+                    <h2 class="text-gray-800 text-2xl font-bold">
+                        <span class="inline-block h-5 border-l-3 border-orange-500 mr-2 mt-5"></span> Selected Properties     
+                        </h2>
+
+
+                
+
+                
+                </div>
+            </div>
+
+
+            <HouseBlock/>
+            <Pagination/>
+
+
+
+
+
+
+
+    
+</main>      
+
+);
+}
+export default GuestSelectedProperties
---
+++ src/pages/propertyDetails.js
@@ -0,0 +1,179 @@
+import React from "react";
+import{useRouter} from 'next/router'
+import UploadImage from '@/components/UploadImage/UploadImage';
+
+
+const PropertyDetails = () => {
+    const router = useRouter()
+    const handleClick=()=>{
+        router.push('/view')
+    }
+    return (
+        <main>
+        <ol class="mx-56 mt-10 mb-10  flex  items-center w-auto">
+<li class="flex ml-20 w-full items-center text-black dark:text-orange-500 after:content-[''] after:w-full after:h-1 after:border-b 
+after:border-amber-100 after:border-4 after:inline-block dark:after:border-amber-600">
+    <span class="flex items-center justify-center w-10 h-10 bg-amber-600 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+    class="w-6 h-6 bg-amber-600 ">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+    </svg>
+
+    </span>
+</li>
+<li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-amber-700">
+    <span class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+    </svg>
+
+    </span>
+</li>
+<li class="flex w-full items-center after:content-[''] after:w-full after:h-1 
+after:border-b after:border-gray-100 after:border-4 after:inline-block 
+dark:after:border-gray-700">
+    <span class="flex items-center justify-center w-10 h-10 bg-gray-100
+    rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+    </svg>
+
+    </span>
+</li>
+<li class="flex items-center w-full">
+    <span class="flex items-center justify-center w-10 h-10 bg-gray-100
+     rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+</svg>
+    </span>
+</li>
+</ol>
+
+    <div className="bg-gray-100  dark:bg-gray-800 transition-colors duration-300" />
+    <div className="container mx-auto p-4">
+        <div className="bg-white dark:bg-gray-700 shadow rounded-lg p-6">
+            <h1 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">Category</h1>
+        
+            <div className=" justify-start grid grid-cols-1 md:grid-cols-4 gap-2 mb-3">
+    < form className="max-w-sm flex justify-start py-5">
+    <select id="countries" className=" block mb-2 text-sm text-gray-600 dark:text-gray-400 bg-white border border-gray-300  rounded-lg    w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option defaultValue>Housing</option>
+    <option value="US">United States</option>
+    <option value="CA">Canada</option>
+    <option value="FR">France</option>
+    <option value="DE">Germany</option>
+    </select>
+    </form>
+
+    < form className="max-w-sm mx-auto py-5">
+    <select id="countries" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option defaultValue>Sale</option>
+    <option value="US">United States</option>
+    <option value="CA">Canada</option>
+    <option value="FR">France</option>
+    <option value="DE">Germany</option>
+    </select>
+    </form>
+    </div>
+    </div>
+    </div>
+
+    <div className="bg-white dark:bg-gray-800 transition-colors duration-300" />
+    <div className="container mx-auto p-4">
+        <div className="bg-white dark:bg-gray-700 shadow rounded-lg p-6">
+            <h1 className="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">General Information</h1>
+        
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Title" className=" text-lg border p-2 rounded w-full" />
+                </div>
+                <div className="mb-4">
+                        <label for="message" className="block mb-2 text-lg text-gray-600 dark:text-gray-400">Description</label>
+
+                        <textarea rows="5" name="message" id="message" placeholder="descripe your home" className="text-lg border p-2 rounded w-full " required></textarea>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="price" class="text-lg border p-2 rounded w-full" />                    <div className="justify-start grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
+    
+                    </div>
+                    </div>
+                
+
+
+                <div className="justify-start grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
+    <form className="max-w-sm flex justify-start">
+    <select id="countries" className=" block mb-2 text-lg text-gray-400 dark:text-gray-400 bg-white border border-gray-300 rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option defaultValue>Type</option>
+    <option value="D">Duplex</option>
+    <option value="G">Garden Apartment</option>
+    <option value="T">Triplex</option>
+    <option value="S">Studio</option>
+    </select>
+    </form>
+
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Number of rooms" className="border p-2 rounded w-full required" />
+                    <input type="text" placeholder="Number of living rooms" className="border p-2 rounded w-full" />
+
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Gross M²" className="border p-2 rounded w-full" />
+                    <input type="text" placeholder="Net M²" className="border p-2 rounded w-full" />
+
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Warming Type" className="border p-2 rounded w-full" />
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Building Age" className="border p-2 rounded w-full" />
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Floor Location" className="border p-2 rounded w-full" />
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Street address" className="border p-2 rounded w-full" />
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                    <input type="text" placeholder="Rental income" className="border p-2 rounded w-full required" />
+                    <input type="text" placeholder="Type" className="border p-2 rounded w-full" />
+
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+                    <input type="text" placeholder="City" className="border p-2 rounded w-full"/>
+                    <input type="text" placeholder="State / Province" className="border p-2 rounded w-full" />
+                    <input type="text" placeholder="ZIP / Postal code" className="border p-2 rounded w-full" />
+                </div>
+
+        
+
+                <UploadImage/>
+                
+
+
+                <button onClick={handleClick}  type="button" id="theme-toggle" class="px-5 mt-10 text-center justify-items-end py-2 rounded bg-amber-600 text-white hover:bg-amber-800 focus:outline-none transition-colors">
+                Next
+            </button>
+            </div>
+            </div>
+
+
+
+
+            
+
+
+
+
+</main>                        
+
+    );
+
+    }
+    export default PropertyDetails
