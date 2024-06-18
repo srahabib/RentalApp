547411e6114a158a9c488a3ab4eb625861a6127a
amany-123 <104681321+amany-123@users.noreply.github.com>
2024-06-18 19:36:24 +0300

    Add files via upload

---
+++ src/pages/DashOwner.js
@@ -0,0 +1,11 @@
+import Dashboard from '../components/DashOwner/Dashowner';
+import Layout from '../components/DashOwnerLayout/Layout';
+
+
+export default function Index() {
+  return (
+    <Layout title='Home Layout'>
+        <Dashboard />
+    </Layout>
+  )
+}
---
+++ src/pages/DashOwnerCategory.js
@@ -0,0 +1,11 @@
+import Category from '../components/DashOwnerCategory/Category';
+import Layout from '../components/DashOwnerLayout/Layout';
+
+
+export default function categoryPage() {
+  return (
+    <Layout title='Category'>
+        <Category />
+    </Layout>
+  )
+}
