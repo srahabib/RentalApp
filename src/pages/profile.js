b6ff7d7f215fcbcc3d5dfe310642a6585cd6ed99
Rewan <143016191+RewanRamadan@users.noreply.github.com>
2024-06-18 06:01:30 +0300

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,32 +1,39 @@
 import React from 'react'
-import ProfileCard1 from '@/components/ProfileCard1/ProfileCard1'
 import ProfileCard from '@/components/ProfileCard/ProfileCard'
-import { HouseCard } from '@/components'
+import Profile from './../components/Profile/Profile';
+import HouseCard from '@/components/HouseCard/HouseCard'
+
 const profile = () => {
     return (
     <div>
-    <ProfileCard1/>
+    <ProfileCard/>
+
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-    <div className="bg-white  rounded ">
-        <ProfileCard/>
+    <div className="bg-white p-4 rounded shadow">
+        <Profile/>
+        </div>
+        <div className="bg-white p-4 rounded shadow max-h-screen overflow-y-auto">
+        <h1 className="text-xl font-semibold mb-4">Recent rented property</h1>
+        <HouseCard />
+        <div/>
     </div>
-    <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg p-6 h-96 overflow-y-auto">
-    <h1 className="text-xl font-semibold mb-4">Recent rented Property</h1>
+    <div className="bg-white p-4 rounded shadow max-h-screen overflow-y-auto">
+    <h1 className="text-xl font-semibold">Rented properties</h1>
+    <p className="text-md text-right font-semibold text-gray-700">2/2/2024</p>
     <HouseCard />
+    <p className="text-md text-right font-semibold text-gray-700">2/3/2023</p>
+    <HouseCard />
+    <p className="text-md text-right font-semibold text-gray-700">1/5/2022</p>
+    <HouseCard />
+
+
+    </div>
     </div>
 
-    <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg p-6 h-96 overflow-y-auto">
-    <h1 className="text-xl font-semibold mb-4">rented Properties</h1>
-    <p className="text-md text-gray-700 font-semibold text-right mt-2">2/2/2023</p>
-    <HouseCard />
-    <hr/>
-    <p className="text-md text-gray-700 font-semibold text-right mt-2">3/8/2022</p>
-    <HouseCard />
-    <hr/>
+
     </div>
-    </div>
-</div>
-    )
+
+)
 }
 
 export default profile
