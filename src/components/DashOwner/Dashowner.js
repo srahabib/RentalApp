76fd58c10e18ff5f8cd7fd6466634b34e82d18f0
amany-123 <104681321+amany-123@users.noreply.github.com>
2024-06-21 03:47:24 +0300

    Update Dashowner.js

--- src/components/DashOwner/Dashowner.js
+++ src/components/DashOwner/Dashowner.js
@@ -18,7 +18,7 @@ const sampleData = {
       label: 'Profit',
       data: [5000, 7000, 8000, 10000, 20000, 25000, 50000, 55000, 60000, 30000, 40000, 45000],
       fill: false,
-      borderColor: 'rgb(255, 179, 0)',
+      borderColor: 'rgb(18, 55, 42)',
       tension: 0.1,
     },
   ],
@@ -49,7 +49,7 @@ const Dashboard = () => {
 
         <section className="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
           <div className="flex items-center p-8 bg-white shadow rounded-lg">
-            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-amber-600 rounded-full mr-6">
+            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-green1 rounded-full mr-6">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -62,7 +62,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="flex items-center p-8 bg-white shadow rounded-lg">
-            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-amber-600 rounded-full mr-6">
+            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-green1 rounded-full mr-6">
               <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-10 w-10">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
               </svg>
@@ -73,7 +73,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="flex items-center p-8 bg-white shadow rounded-lg">
-            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-amber-600 rounded-full mr-6">
+            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-green1 rounded-full mr-6">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
@@ -85,7 +85,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="flex items-center p-8 bg-white shadow rounded-lg">
-            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-amber-600 rounded-full mr-6">
+            <div className="inline-flex flex-shrink-0 items-center justify-center h-20 w-20 text-white bg-green1 rounded-full mr-6">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
             <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
           </svg>          
